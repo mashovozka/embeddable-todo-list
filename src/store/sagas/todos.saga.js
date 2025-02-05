0afdfbfe40c7c9f1f748addb535f090f8152b1a1
mariia.tkachova <mashovozka@gmail.com>
2025-02-05 22:09:26 +0100

    added saga

---
+++ src/store/sagas/todos.saga.js
@@ -0,0 +1,12 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+import { TODOS_ACTION_TYPES, setTodos } from "../todos/todos.action";
+import { todosApi } from "../../api/api";
+
+export function* todosSaga() {
+  yield takeEvery(TODOS_ACTION_TYPES.GET_TODOS, function* () {
+    try {
+      const response = yield call(todosApi.getTodos);
+      yield put(setTodos(response.data));
+    } catch (error) {}
+  });
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,19 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas/root-saga";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  sagaMiddleware,
+  process.env.NODE_ENV === "development" && logger,
+].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+sagaMiddleware.run(rootSaga);
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
