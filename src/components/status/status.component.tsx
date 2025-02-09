031a5b86bb1aeb684a32978fa6fc03d5d3808abc
mariia.tkachova <mashovozka@gmail.com>
2025-02-09 14:07:36 +0100

    added status folder

---
+++ src/components/status/status.component.tsx
@@ -0,0 +1,18 @@
+import { StatusContainer } from "./status.styles";
+import { useAppSelector } from "@src/store/hooks";
+
+const Status = () => {
+  const todos = useAppSelector((state) => state.todos.todos);
+
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const inProgressTodos = todos.length - completedTodos;
+
+  return (
+    <StatusContainer>
+      <div>In Progress: {inProgressTodos}</div>
+      <div>Completed: {completedTodos}</div>
+    </StatusContainer>
+  );
+};
+
+export default Status;
